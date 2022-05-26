c1c842a533ef07e24009d0ccee186a211627dc4a
bnbong <bbbong9@gmail.com>
2022-05-26 21:06:54 +0900

    solved  10699

---
+++ learning_Python/prob_10699.py
@@ -0,0 +1,32 @@
+from time import *
+
+def prob_10699():
+    day_of_name, month, day, time, year = asctime(gmtime()).split()
+
+    if month == "Jan":
+        month = "01"
+    elif month == "Feb":
+        month = "02"
+    elif month == "Mar":
+        month = "03"
+    elif month == "Apr":
+        month = "04"
+    elif month == "May":
+        month = "05"
+    elif month == "Jun":
+        month = "06"
+    elif month == "Jul":
+        month = "07"
+    elif month == "Aug":
+        month = "08"
+    elif month == "Sep":
+        month = "09"
+    elif month == "Oct":
+        month = "10"
+    elif month == "Nov":
+        month = "11"
+    elif month == "Dec":
+        month = "12"
+
+    print(f'{year}-{month}-{day}')
+prob_10699()
