996ce5682c8fe87182a233efe1dbb34a2df8c080
bnbong <bbbong9@gmail.com>
2022-10-19 04:26:54 +0000

    solved 2914

---
+++ learning_Python/prob_2914.py
@@ -0,0 +1,10 @@
+def prob_2914():
+    # a, b = input().split(" ")
+    # a = int(a)
+    # b = int(b)
+    a, b = map(int, input().split(" "))
+    result = a * (b - 1) + 1
+
+    print(result)
+
+prob_2914()
