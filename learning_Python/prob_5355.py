9083b1badcea8da4b0ca91807d72cd841893d57c
bnbong <bbbong9@gmail.com>
2022-10-24 05:48:38 +0000

    solved 5355

---
+++ learning_Python/prob_5355.py
@@ -0,0 +1,24 @@
+def prob_5355():
+    cases = int(input())
+    results = []
+
+    for case in range(0, cases):
+        calculate = input().split(" ")
+        
+        number = float(calculate[0])
+        calculate = calculate[1:]
+
+        for item in calculate:
+            if item == "@":
+                number = number * 3.00
+            if item == "%":
+                number = number + 5.00
+            if item == "#":
+                number = number - 7.00
+        
+        results.append(number)
+
+    for number in results:
+        print(round(number, 2))
+
+prob_5355()
