4439afd9a74eed02f01824fea18204622630b998
bnbong <bbbong9@gmail.com>
2022-10-31 00:10:42 +0000

    solved 2675

--- learning_Python/prob_2675.py
+++ learning_Python/prob_2675.py
@@ -1,4 +1,21 @@
+def repeat_string(repeat_time, string):
+    result_string = ""
+    for i in range(0, len(string)):
+        result_string += string[i] * repeat_time
+
+    return result_string
+
 def prob_2675():
-    pass
+    test_limits = int(input())
+    results = []
+    
+    for case in range(0, test_limits):
+        repeat_time, string = input().split(" ")
+        repeat_time = int(repeat_time)
+        new_string = repeat_string(repeat_time, string)
+        results.append(new_string)
+    
+    for result in results:
+        print(result)
 
 prob_2675()
