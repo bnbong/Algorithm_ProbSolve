f5679b82d0d35940ccd5ea459cb253c01f50b575
bnbong <bbbong9@gmail.com>
2022-11-02 12:01:54 +0900

    solved 2935

--- learning_Python/prob_2935.py
+++ learning_Python/prob_2935.py
@@ -1,4 +1,15 @@
 def prob_2935():
-    pass
+    first_number = int(input())
+    operator = input()
+    second_number = int(input())
+
+    result = 0
+    if operator == "*":
+        result = first_number * second_number
+    else:
+        result = first_number + second_number
+
+    print(result)
+
 
 prob_2935()
