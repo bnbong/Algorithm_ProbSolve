225914e73c010d28e58141b6c3bd6fbb0b75477c
bnbong <bbbong9@gmail.com>
2022-11-06 15:24:32 +0900

    solved 11653

--- learning_Python/prob_11653.py
+++ learning_Python/prob_11653.py
@@ -1,4 +1,16 @@
+def factorization(x):
+    d = 2
+
+    while d <= x:
+        if x % d == 0:
+            print(d)
+            x = x / d
+        else:
+            d = d + 1
+
 def prob_11653():
-    pass
+    input_number = int(input())
+    result = factorization(input_number)
+
 
 prob_11653()
