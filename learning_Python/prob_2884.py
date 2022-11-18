ae8e1494d6057a50596dc0e40c38b4d7fc91fb60
bnbong <bbbong9@gmail.com>
2022-11-18 06:18:34 +0000

    solved 2884

---
+++ learning_Python/prob_2884.py
@@ -0,0 +1,11 @@
+def prob_2884():
+    H, M = map(int, input().split())
+
+    if M > 44:
+        print(H, M - 45)
+    elif M < 45 and H > 0:
+        print(H - 1, M + 15)
+    else:
+        print(23, M + 15)
+
+prob_2884()
