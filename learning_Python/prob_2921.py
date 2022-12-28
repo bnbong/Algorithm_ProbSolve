5174ac22c37d611a7f56195c14b1190ff0663d9e
bnbong <bbbong9@gmail.com>
2022-12-28 01:39:07 +0000

    solved 2921

--- learning_Python/prob_2921.py
+++ learning_Python/prob_2921.py
@@ -1,4 +1,9 @@
 def prob_2921():
-    pass
+    n = int(input())
+    result = 0
+    for i in range(0, n + 1):
+        for j in range(i, n + 1):
+            result += i + j
+    print(result)
 
 prob_2921()
