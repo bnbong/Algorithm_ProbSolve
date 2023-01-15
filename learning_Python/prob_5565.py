a1ab360bf4eb71e2bea7dbc209af73eb782208c5
bnbong <bbbong9@gmail.com>
2023-01-15 03:34:50 +0000

    solved 5565

--- learning_Python/prob_5565.py
+++ learning_Python/prob_5565.py
@@ -1,4 +1,12 @@
 def prob_5565():
-    pass
+    sum_of_book = int(input())
+    result = 0
+    for _ in range(9):
+        price_of_book = int(input())
+        result += price_of_book
+    
+    result = sum_of_book - result
+    print(result)
+
 
 prob_5565()
