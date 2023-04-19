ab094e5f8a7b62c37f89c426480569df198eaef6
이준혁 <bbbong9@gmail.com>
2023-04-19 21:37:20 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0: # 1사분면
+    print(1)
+elif x < 0 and y > 0: # 2사분면
+    print(2)
+elif x < 0 and y < 0: # 3사분면
+    print(3)
+elif x > 0 and y < 0: # 4분면
+    print(4)
+    
