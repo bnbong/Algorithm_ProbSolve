5d9b0f2548d70182a0a27896fdf04e3fab78c0ad
이준혁 <bbbong9@gmail.com>
2026-04-03 16:26:49 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,7 +1,6 @@
 S = input()
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-for i in alpha:
-    if i in S:
-        print(S.index(i), end=' ')
-    else:
-        print(-1, end=' ')
+
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for a in alpha:
+	print(S.find(a), end=' ')
